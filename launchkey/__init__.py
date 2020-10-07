a31946543a7211907356e72d452ed7d6cb2ea658
Branden Jordan <branden.jordan@iovation.com>
2020-10-06 17:15:45 -0700

    incremented rc version to 2 (#105)

--- launchkey/__init__.py
+++ launchkey/__init__.py
@@ -1,5 +1,5 @@
 """LaunchKey Service SDK module"""
-SDK_VERSION = '3.8.1-rc.1'
+SDK_VERSION = '3.8.1-rc.2'
 LAUNCHKEY_PRODUCTION = "https://api.launchkey.com"
 VALID_JWT_ISSUER_LIST = ["svc", "dir", "org"]
 JOSE_SUPPORTED_JWE_ALGS = ['RSA-OAEP']
