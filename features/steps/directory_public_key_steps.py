83b2068b10db947559c043a7e46ba91e812a8abd
Juan Rodriguez <juan.rodriguez@iovation.com>
2021-10-25 16:04:29 -0700

    Revision #2 - Addition of helper functions in order to convert strings within Enum into values

--- features/steps/directory_public_key_steps.py
+++ features/steps/directory_public_key_steps.py
@@ -213,17 +213,29 @@ def verify_directory_public_key_is_in_list_of_public_keys(context):
     raise Exception("Unable to find the current directory public key")
 
 
+def string_to_key_type(rawkeytype):
+    switch = {
+        "BOTH": 0,
+        "ENCRYPTION": 1,
+        "SIGNATURE": 2
+    }
+    return switch.get(rawkeytype, -1)
+
+
 @then("the Public Key is in the list of Public Keys for the Directory and has "
       "a {key_type} key type")
 @then("the Public Key is in the list of Public Keys for the Directory and has "
       "a \"{key_type}\" key type")
 def verify_directory_public_key_is_in_list_of_public_keys(context, key_type):
-    key_type_enum = KeyType(str(key_type))
+    if isinstance(key_type, str):
+        key_type_enum = KeyType(string_to_key_type(key_type))
+    else:
+        key_type_enum = KeyType(int(key_type))
     alpha_public_key = context.keys_manager.alpha_public_key
     current_directory_public_keys = context.entity_manager. \
         get_current_directory_public_keys()
     for key in current_directory_public_keys:
-        if key.public_key == alpha_public_key and key.key_type == key_type_enum:
+        if key.public_key == alpha_public_key and key.string_to_key_type(key_type) == key_type_enum:
             return
     raise Exception("Unable to find the current directory public key")
 
@@ -257,7 +269,7 @@ def verify_directory_public_key_is_inactive(context):
 @then("the Directory Public Key Expiration Date is "
       "\"{expiration_timestamp}\"")
 def verify_directory_public_key_expiration_date(context,
-                                                        expiration_timestamp):
+                                                expiration_timestamp):
     key_id = context.keys_manager.alpha_md5_fingerprint
     current_directory_public_keys = context.entity_manager. \
         get_current_directory_public_keys()
