573f2f8121c5505e23350c5e35f24e138b14fc04
Brad Porter <brad.porter@iovation.com>
2022-01-31 14:20:29 -0700

    Bumped version to 4.1

--- launchkey/__init__.py
+++ launchkey/__init__.py
@@ -1,5 +1,5 @@
 """LaunchKey Service SDK module"""
-SDK_VERSION = '4.0.0'
+SDK_VERSION = '4.1.0'
 LAUNCHKEY_PRODUCTION = "https://api.launchkey.com"
 VALID_JWT_ISSUER_LIST = ["svc", "dir", "org"]
 JOSE_SUPPORTED_JWE_ALGS = ['RSA-OAEP']
